a5c2245c91a5b90492b9b8df7bbca3cd2bf0a5a2
kkiyama117 <k.kiyama117@gmail.com>
2018-10-09 15:42:08 +0900

    Update main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
 from get_data import get_data
+import panda as pd
 
+def data_to_pd():
+    # 為替レートのdictをDataFrameへ変換
+    rate = pd.DataFrame.from_dict(
+        {r.response['candles'][i]['time']: r.response['candles'][i]['mid']
+         for i in range(0, len(r.response['candles']))
+         for j in r.response['candles'][i]['mid'].keys()},
+        orient='index',
+        )
 if __name__ == '__main__':
     print(get_data())
